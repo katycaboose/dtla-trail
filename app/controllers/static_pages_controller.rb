701f60d910e2c96b31733ac37c9a31e0594f41fa
katycaboose <ktmccreery@gmail.com>
2016-04-30 13:21:51 -0700

    updates to static page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
     puts
-    "This is the beginning of your journey back to your party. Use the clues to navigate to each destination. At the destination there will be a red envelopes in each location containing a question. The correct answer to each question will unlock the next clue. If you reach the end of your journey you will be reunited with your party. Enter your full name to get your first clue"
+    "This is the beginning of your journey back to your party. Use the clues to navigate to each destination. At the destination there will be a red envelopes in each location containing a question. The correct answer to each question will unlock the next clue. If you reach the end of your journey you will be reunited with your party. Enter your full name to get your first clue."
   	if params[:answer]
     	@string = Clues.new(params[:answer])
     	@string.array_counter
     	@response = @string.show_answer
     end
+  end
 end
 
   def help
