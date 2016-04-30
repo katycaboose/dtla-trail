572d97f02698e940f65c2d3a2aae1ca0481c82a4
katycaboose <ktmccreery@gmail.com>
2016-04-30 13:17:12 -0700

    edit the home page
    - remove the escape lines
    - don’t need the first question

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
     puts
-    "This is the beginning of your journey back to your party.\nUse the clues to navigate to each destination.\nThere will be red envelopes in each location containing a question.\nThe correct answer to each question will unlock the next clue.\nAny wrong answers will weaken your player.\nIf you reach the end of your journey you will be reunited with your party.\nEnter your FULL NAME below to get your first clue."
+    "This is the beginning of your journey back to your party. Use the clues to navigate to each destination. At the destination there will be a red envelopes in each location containing a question. The correct answer to each question will unlock the next clue. If you reach the end of your journey you will be reunited with your party. Enter your full name to get your first clue"
   	if params[:answer]
     	@string = Clues.new(params[:answer])
     	@string.array_counter
     	@response = @string.show_answer
-    else
-      @first_question = "What is your full name?"
-  end
+    end
 end
 
   def help
